cfcc28f9889c7c137a93833bc878c47f4f42356d
Archn3m3sis <kyle@archn3m3sis.com>
2025-07-04 16:40:29 -0400

    added rxconfig database url

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="cyber_industrial",
+    db_url="sqlite:///cyber_industrial/database/data/cyber_industrial.db",
     plugins=[
-        rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
 )
